7d7bc57fc633029fad565d5d0fb22943b11ded4e
John Achor <johnachor@gmail.com>
2019-02-06 23:52:25 -0600

    interface declarations added

---
+++ WhoseArrows/DBAccess/Interface/IPlayerAccess.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WhoseArrows.Models.DB;
+
+namespace WhoseArrows.DBAccess.Interface
+{
+	interface IPlayerAccess
+	{
+		// Single DB object insertion methods
+		Task<Player>			AddNewPlayer			(Player newPlayer);
+		Task<Session>			AddNewSession			(Session newSession);
+		Task<SessionQuestion>	AddNewSessionQuestion	(SessionQuestion newSessionQuestion);
+
+		// Deletions
+		Task<Player>	DeletePlayer	(long playerId);
+		Task<Session>	DeleteSession	(long sessionId);
+
+		// Entry points for main operations
+		Task<Question>	PlayerGuess			(long sessionQuestionId, long songId);
+		Task<Hint>		GetHint				(long sessionQuestionId);
+		Task<Session>	AddAnonPlayerName	(long sessionId, string anonPlayerName);
+		Task<Session>	AddPlayerIdToSession(long sessionId, long playerId);
+	}
+}
